e387c270abcdfd1be13c67709614ad080574d48a
Marcelo Maia <mmaia.cc@gmail.com>
2019-02-28 16:21:23 -0300

    refatoração de banco 1

--- chatbots.py
+++ chatbots.py
@@ -8,6 +8,7 @@ from enum import Enum
 import requests
 
 from api_client import PratoAbertoApiClient
+from mongocon import BotDbConnection
 
 log = logging.getLogger(__name__)
 
@@ -21,14 +22,13 @@ class BotFlowEnum(Enum):
 
 class BaseBot(object):
 
-    def __init__(self, payload, conn):
+    def __init__(self, payload):
         self.payload = payload
-        self.conn = conn
 
     def send_message(self, text, keyboard_opts=None):
         raise NotImplementedError
 
-    def update_user_data(self, args):
+    def update_flow_data(self, args):
         raise NotImplementedError
 
     def set_flow(self, flow_name, step):
@@ -47,27 +47,18 @@ class TelegramBot(BaseBot):
         It doesnt handle states
     """
 
-    def __init__(self, payload, conn):
-        super().__init__(payload, conn)
+    def __init__(self, payload):
+        super().__init__(payload)
         TG_URL = 'https://api.telegram.org/bot{}/'.format(os.environ.get('TG_TOKEN'))
         self.TG_BASE_MESSAGE_URL = TG_URL + 'sendMessage?chat_id={}&text={}&parse_mode=Markdown'
         self.chat_id = payload['message']['chat']['id']
         self.text = payload['message']['text'].strip()
         self.chat_name = payload['message']['chat']['first_name']
         self._check_flow()
+        self.user_conn = BotDbConnection(self.chat_id, 'telegram', name=self.chat_name)
 
     def clear_data(self):
-        query = {'_id': self.chat_id}
-        user = self.conn.users.find_one(query)
-        if user:
-            args = {'school': '',
-                    'age': '',
-                    'menu_date': '',
-                    'evaluation': '',
-                    'last_selected_meal': '',
-                    'satisfied': '',
-                    'opnion': ''}
-            self.conn.users.update_one(query, {'$set': args}, upsert=True)
+        self.user_conn.clean_flow_control_data()
 
     def send_message(self, text, keyboard_opts=None):
         """
@@ -91,75 +82,23 @@ class TelegramBot(BaseBot):
         return r.json()
 
     def save_notification(self):
-        user_data = self.get_user_data()
-        if user_data:
-            self._create_notification(user_data)
+        self.user_conn.save_notification()
 
-    def update_user_data(self, args):
-        """
-        Generic method, to update or create fields in user collection
-        :param args: a dict
-        """
-        query = {'_id': self.chat_id}
-        user = self.conn.users.find_one(query)
-        if not user:
-            self._create_user()
-        self.conn.users.update_one(query, {'$set': args}, upsert=True)
+    def update_flow_data(self, **kwargs):
+        self.user_conn.update_flow_control(**kwargs)
 
     def get_user_data(self):
-        """
-        :return: dict
-        """
-        query = {'_id': self.chat_id}
-        user = self.conn.users.find_one(query)
-        return user
+        return self.user_conn.to_dict()
 
-    def set_flow(self, flow_name, step):
-        query = {'_id': self.chat_id}
-        user = self.conn.users.find_one(query)
-        if not user:
-            self._create_user()
-        flow_info = {
-            'flow_name': flow_name,
-            'step': step
-        }
-        self.conn.users.update_one(query, {'$set': flow_info}, upsert=True)
+    def set_flow(self, flow_name, flow_step):
+        self.user_conn.update_flow_control(flow_name=flow_name, flow_step=flow_step)
 
     def concat_evaluation(self):
-        query = {'_id': self.chat_id}
-        user = self.get_user_data()
-        assert user['flow_name'] == BotFlowEnum.AVALIAR_REFEICAO.value
-        evaluation_data = {'school': user['school'],
-                           'meal': user['last_selected_meal'],
-                           'age': user['age'],
-                           'evaluation': user['evaluation'],
-                           'opnion': user['opnion'],
-                           'satisfied': user['satisfied'],
-                           'menu_date': user['menu_date']}
-        self.conn.users.update_one(query, {'$push': {'evaluations': evaluation_data}}, upsert=True)
+        self.user_conn.save_evaluation()
 
     #
     # Private
     #
-    # TODO: separar as operações de banco da classe do telegram, não faz sentido ficar aqui.
-
-    def _concat_evaluation(self, user_data):
-        query = {'_id': self.chat_id}
-        age = user_data['age']
-        school = user_data['school']
-        # TODO: registrar a refeição escolhida...
-        meal = user_data['meal']
-
-    def _create_notification(self, user_data):
-        query = {'_id': self.chat_id}
-        age = user_data['age']
-        school = user_data['school']
-        flow_name = user_data['flow_name']
-        args = {'age': age,
-                'school': school,
-                'source': 'telegram'}
-        assert flow_name == BotFlowEnum.RECEBER_NOTIFICACAO.value
-        self.conn.notifications.update_one(query, {'$set': args}, upsert=True)
 
     def _check_flow(self):
         """if text is equal to initial statuses, them back to begin."""
@@ -169,17 +108,7 @@ class TelegramBot(BaseBot):
             self._reset_flow(self.text)
 
     def _reset_flow(self, text):
-        self.set_flow(flow_name=text, step=0)
-
-    def _create_user(self):
-        query = {'_id': self.chat_id}
-        user = self.conn.users.find_one(query)
-        if not user:
-            user_info = {
-                'name': self.chat_name,
-                'source': 'telegram'
-            }
-            self.conn.users.update_one(query, {'$set': user_info}, upsert=True)
+        self.set_flow(flow_name=text, flow_step=0)
 
     def _concat_buttons(self, keyboard_opts, url, show_once=True):
         # https://core.telegram.org/bots/api/#keyboardbutton
@@ -240,11 +169,11 @@ class EduBot(object):
                  'Todas as idades',
                  '1 ano']
 
-    def __init__(self, platform, payload, conn):
+    def __init__(self, platform, payload):
         log.debug('{} -> payload: {}'.format(platform, payload))
         self.api_client = PratoAbertoApiClient()
         if platform == 'telegram':
-            self.bot = TelegramBot(payload, conn)
+            self.bot = TelegramBot(payload)
 
     def process_flow(self):
         user_data = self.bot.get_user_data()
@@ -297,17 +226,17 @@ class EduBot(object):
             self._show_menu(has_buttons=True)
             self.bot.set_flow(current_flow, self.STEP_MENU_SHOWN2)
         elif step == self.STEP_MENU_SHOWN2:
-            self.bot.update_user_data({'last_selected_meal': self.bot.text})
+            self.bot.update_flow_data({'last_selected_meal': self.bot.text})
             self.bot.send_message('Satisfeito com as refeições?', keyboard_opts=self.yesno_opts)
             self.bot.set_flow(current_flow, self.STEP_SATISFIED)
         elif step == self.STEP_SATISFIED:
             # TODO: validator de input y/n
-            self.bot.update_user_data({'satisfied': self.bot.text})
+            self.bot.update_flow_data({'satisfied': self.bot.text})
             self.bot.send_message('O que achou da refeição?', keyboard_opts=self.evaluation_opts)
             self.bot.set_flow(current_flow, self.STEP_EVALUATION)
         elif step == self.STEP_EVALUATION:
             # TODO: validador de opções
-            self.bot.update_user_data({'evaluation': self.bot.text})
+            self.bot.update_flow_data({'evaluation': self.bot.text})
             self.bot.send_message('Gostaria de deixar alguma opnião?', keyboard_opts=self.yesno_opts)
             self.bot.set_flow(current_flow, self.STEP_HAS_OPINION)
         elif step == self.STEP_HAS_OPINION:
@@ -319,7 +248,7 @@ class EduBot(object):
                 self.bot.send_message('Digite abaixo sua opinião...')
                 self.bot.set_flow(current_flow, self.STEP_OPINION)
         elif step == self.STEP_OPINION:
-            self.bot.update_user_data({'opnion': self.bot.text})
+            self.bot.update_flow_data({'opnion': self.bot.text})
             self.bot.concat_evaluation()
             self.bot.send_message('Obrigado!')
             self._main_menu()
@@ -334,7 +263,7 @@ class EduBot(object):
             self._search_school(current_flow)
         elif step == self.STEP_SCHOOL_SELECTED:
             if self._is_age_option(self.bot.text):
-                self.bot.update_user_data(args={'age': self.bot.text})
+                self.bot.update_flow_data(args={'age': self.bot.text})
                 self.bot.save_notification()
                 self.bot.send_message('Sua notificação foi salva!')
                 self._main_menu()
@@ -417,11 +346,11 @@ class EduBot(object):
         return retval
 
     def _school_selected_parse_age(self):
-        self.bot.update_user_data(args={'age': self.bot.text})
+        self.bot.update_flow_data(args={'age': self.bot.text})
         self.bot.send_message('Escolha o dia', self.days_opts)
 
     def _school_selected_parse_day(self, current_flow):
-        self.bot.update_user_data(args={'menu_date': self._parse_date(self.bot.text)})
+        self.bot.update_flow_data(args={'menu_date': self._parse_date(self.bot.text)})
         if current_flow == BotFlowEnum.QUAL_CARDAPIO.value:
             self._show_menu()
             self._main_menu()
@@ -430,7 +359,7 @@ class EduBot(object):
             self._flow_evaluate_meal(self.STEP_MENU_SHOWN)
 
     def _school_selected_get_ages(self):
-        self.bot.update_user_data(args={'school': self.bot.text})
+        self.bot.update_flow_data(args={'school': self.bot.text})
         ages = self.api_client.get_ages_by_school_nome(self.bot.text)
         if ages:
             self.bot.send_message('Escolha uma idade', ages)
@@ -438,7 +367,7 @@ class EduBot(object):
     def _search_school(self, current_flow):
         schools = self._get_schools_by_name(self.bot.text)
         if schools:
-            self.bot.set_flow(current_flow, step=self.STEP_SCHOOL_SELECTED)
+            self.bot.set_flow(current_flow, flow_step=self.STEP_SCHOOL_SELECTED)
             self.bot.send_message('Escolha uma escola', schools)
         else:
             self.bot.send_message('Nenhuma escola encontrada')
